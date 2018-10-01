cb21f5ed911e89d28e4e313f46181b0da5b7f4e5
fred <fredlahde@gmail.com>
2018-10-01 18:52:00 +0200

    start impl slab

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+#![feature(alloc, allocator_api)]
+#![feature(const_fn)]
+#![no_std]
+
+extern crate std;
+extern crate alloc;
+extern crate linked_list_allocator;
+
+mod slab;
+
+pub const MIN_SLAB_SIZE: usize = 4096;
+pub const PAGE_SIZE: usize = MIN_SLAB_SIZE;
+pub const NUM_OF_SLABS: usize = 8;
+pub const MIN_HEAP_SIZE: usize = NUM_OF_SLABS * MIN_SLAB_SIZE;
+
 #[cfg(test)]
 mod tests {
     #[test]
